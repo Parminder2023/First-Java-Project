06876a4976bf3ddad5126c9816bc96600d8e7bf6
Parminder2023 <raiparminder3663@.com>
2023-03-30 16:43:46 -0400

    adding a file

---
+++ src/doWhileLoopDemo/DoWhileLoop.java
@@ -0,0 +1,23 @@
+package doWhileLoopDemo;
+
+public class DoWhileLoop {
+
+	//do while loop
+	
+	int finalNumbers = 10;
+	
+	
+	void Print10SuccessiveNumbers () {
+		
+		int i = 1;
+		
+		do {
+			System.out.println(i);
+			i++;
+			
+		} while (i <= 10);
+		
+		
+	}
+	
+}
---
+++ src/doWhileLoopDemo/doWhileLoopMain.java
@@ -0,0 +1,17 @@
+package doWhileLoopDemo;
+
+public class doWhileLoopMain {
+
+	public static void main(String[] args) {
+		
+		DoWhileLoop dowhileLoop = new DoWhileLoop ();
+		dowhileLoop.finalNumbers = 10;
+		dowhileLoop.Print10SuccessiveNumbers();
+		
+		
+		
+		
+		
+		
+	}
+}
