1466755dc7aaa0bd4730ba7dc75f674a30fc501b
Parminder2023 <raiparminder3663@.com>
2023-04-03 12:56:11 -0400

    week2 assignment

---
+++ src/chocolate/Chocolate.java
@@ -0,0 +1,20 @@
+package chocolate;
+
+public class Chocolate {
+	
+
+	int Chocolates ;
+	int finalNumber ;
+	
+	//Keep adding 5 chocolates to make it 63
+	//while loop
+	
+	    void addChocolateToABox() {
+		while (Chocolates<=finalNumber) {
+		System.out.println(+Chocolates);
+			Chocolates+=5; //chocolates= chocolates+5
+			
+		}
+	}
+
+}
---
+++ src/chocolate/ChocolateMain.java
@@ -0,0 +1,17 @@
+package chocolate;
+
+public class ChocolateMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Chocolate Ch = new Chocolate();
+		Ch.Chocolates = 27;
+		Ch.finalNumber = 63;
+		
+		Ch.addChocolateToABox();
+		
+
+	}
+
+}
