5d67ed46e5ae712c11d5d712195cb06f834a72e5
Parminder2023 <raiparminder3663@.com>
2023-04-03 12:54:16 -0400

    week2 assignment

---
+++ src/calculations/Operations.java
@@ -0,0 +1,33 @@
+package calculations;
+
+public class Operations {
+	
+	int num;
+	int square = num*num;
+	int cube = num*num*num;
+	
+	void squareOfNumber() {
+		int square = num*num;
+	
+	System.out.println(num*num);
+	}
+		
+       void cubeOfNumbers() {
+		  cube = num*num*num ;
+		  System.out.println(num*num*num);
+		  
+        {
+    	   System.out.println("Invalid operation");
+    	   
+        }
+        
+    	  
+       }
+}
+		
+	
+	
+	  
+	
+
+
---
+++ src/calculations/OperationsMain.java
@@ -0,0 +1,18 @@
+package calculations;
+
+public class OperationsMain {
+	
+	public static void main(String [] args) {
+		
+		Operations Op = new Operations ();
+		Op.num = 3;
+		Op.square = 3*3;
+		Op.cube = 3*3*3;
+		Op.squareOfNumber();
+		Op.cubeOfNumbers();
+		
+		
+		
+	}
+
+}
