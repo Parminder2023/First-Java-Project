0604d2fd6e462aa38bc95a9eb71676b8e24467a8
Parminder2023 <raiparminder3663@.com>
2023-04-24 18:57:31 -0400

    adding new file

---
+++ src/week5Assignment/GuessTheStudent.java
@@ -0,0 +1,75 @@
+package week5Assignment;
+
+import java.util.Scanner;
+
+public class GuessTheStudent {
+
+	// words: "_"
+	// points: 5
+	// Input: x
+
+	String[] studentNames = { "aleena", "angel", "asha", "clemin", "gopesh", "hamneet", "jimna", "jobin", "jude",
+			"kamaldeep", "manpreet k", "manpreet s", "mariya", "namitha", "neelam", "nisha", "parminder", "rajat",
+			"rojy", "sojan", "sucharita", "vinduja" };
+
+	String randomWords = studentNames[(int) (Math.random() * studentNames.length)];
+	char[] letters = new char[randomWords.length()];
+	Scanner sc = new Scanner(System.in);
+
+	void GuessNameOfStudent() {
+
+		for (int i = 0; i < letters.length; i++) {
+			letters[i] = '_';
+		}
+		int points = 5;
+
+		while (points > 0) {
+			System.out.println("points: ");
+			for (int i = 0; i < points; i--) {
+				System.out.println("o");
+				break;
+			}
+			System.out.println("Guess a letter: ");
+			String input = sc.nextLine();
+
+			char letter = input.charAt(0);
+			boolean isGuessCorrect = false;
+			for (int count = 0; count < randomWords.length(); count++) {
+				char l = randomWords.charAt(count);
+				if (l == letter) {
+					letters[count] = l;
+					isGuessCorrect = true;
+				}
+			}
+			if (!isGuessCorrect) {
+				points = points - 1;
+			}
+
+			boolean isGameFinished = true;
+
+			System.out.println("You are guessing: ");
+			for (int i = 0; i < letters.length; i++) {
+				if (letters[i] == '_') {
+					isGameFinished = false;
+				}
+
+				System.out.println(letters[i]);
+			}
+
+			System.out.println();
+			if (isGameFinished) {
+				System.out.println("you win!" );
+				System.out.println("you have guessed " + randomWords + " correctly");
+				break;
+
+			}
+		}
+		if (points == 0) {
+			System.out.println("you lost! the word was:" + randomWords);
+
+			System.out.println("Game over");
+
+		}
+
+	}
+}
---
+++ src/week5Assignment/GuessTheStudentMain.java
@@ -0,0 +1,13 @@
+package week5Assignment;
+
+public class GuessTheStudentMain {
+
+	public static void main(String args[]) {
+
+		GuessTheStudent student = new GuessTheStudent();
+		student.GuessNameOfStudent();
+		System.out.println("Starting new game");
+
+	}
+
+}
