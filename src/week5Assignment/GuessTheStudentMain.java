65eec3135052837a5cef8cee64fded95a2f9098a
Parminder2023 <raiparminder3663@.com>
2023-04-25 00:52:30 -0400

    updated a file

--- src/week5Assignment/GuessTheStudentMain.java
+++ src/week5Assignment/GuessTheStudentMain.java
@@ -1,13 +1,16 @@
 package week5Assignment;
 
+import java.util.Scanner;
+
 public class GuessTheStudentMain {
 
 	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
 
 		GuessTheStudent student = new GuessTheStudent();
 		student.GuessNameOfStudent();
 		System.out.println("Starting new game");
-
+		
+		}
 	}
 
-}
