49ac0e9c2d205c2176b6dd7d18274a1f557bd240
Parminder2023 <raiparminder3663@.com>
2023-04-25 00:51:39 -0400

    updated a file

--- src/week5Assignment/GuessTheStudent.java
+++ src/week5Assignment/GuessTheStudent.java
@@ -4,9 +4,10 @@ import java.util.Scanner;
 
 public class GuessTheStudent {
 
-	// words: "_"
+	// words: 
 	// points: 5
 	// Input: x
+	//points : o o o
 
 	String[] studentNames = { "aleena", "angel", "asha", "clemin", "gopesh", "hamneet", "jimna", "jobin", "jude",
 			"kamaldeep", "manpreet k", "manpreet s", "mariya", "namitha", "neelam", "nisha", "parminder", "rajat",
@@ -14,6 +15,7 @@ public class GuessTheStudent {
 
 	String randomWords = studentNames[(int) (Math.random() * studentNames.length)];
 	char[] letters = new char[randomWords.length()];
+
 	Scanner sc = new Scanner(System.in);
 
 	void GuessNameOfStudent() {
@@ -21,18 +23,24 @@ public class GuessTheStudent {
 		for (int i = 0; i < letters.length; i++) {
 			letters[i] = '_';
 		}
+		System.out.print("The word has " + randomWords.length() + " letters. ");
 		int points = 5;
 
-		while (points > 0) {
-			System.out.println("points: ");
-			for (int i = 0; i < points; i--) {
-				System.out.println("o");
+		while (points > 0 ) {
+			System.out.print("points: ");
+			for (int i = 0; i < points; i++) {
+
+				System.out.print("o");
+				
+
 				break;
 			}
-			System.out.println("Guess a letter: ");
-			String input = sc.nextLine();
+			 System.out.println();
+			System.out.print("Guess a letter: ");
 
-			char letter = input.charAt(0);
+			String GuessALetter = sc.nextLine();
+
+			char letter = GuessALetter.charAt(0);
 			boolean isGuessCorrect = false;
 			for (int count = 0; count < randomWords.length(); count++) {
 				char l = randomWords.charAt(count);
@@ -44,32 +52,34 @@ public class GuessTheStudent {
 			if (!isGuessCorrect) {
 				points = points - 1;
 			}
+			
 
 			boolean isGameFinished = true;
 
-			System.out.println("You are guessing: ");
+		
 			for (int i = 0; i < letters.length; i++) {
 				if (letters[i] == '_') {
 					isGameFinished = false;
 				}
 
-				System.out.println(letters[i]);
+				System.out.print(letters[i]);
+			}
+			{
+				System.out.println();
+				if (isGameFinished) {
+					System.out.println("you win!");
+					System.out.println("you have guessed " + randomWords + " correctly");
+					break;
+
+				}
+			}
+			if (points == 0) {
+				System.out.println("you lost! the word was:" + randomWords);
+
+				System.out.println("Game over");
+
 			}
 
-			System.out.println();
-			if (isGameFinished) {
-				System.out.println("you win!" );
-				System.out.println("you have guessed " + randomWords + " correctly");
-				break;
-
-			}
 		}
-		if (points == 0) {
-			System.out.println("you lost! the word was:" + randomWords);
-
-			System.out.println("Game over");
-
-		}
-
 	}
 }
