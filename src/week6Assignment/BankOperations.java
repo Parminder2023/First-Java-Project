6b5a8f5f4e2b2c61c31efcbb210f85bafe86fa38
Parminder2023 <raiparminder3663@.com>
2023-05-08 13:09:35 -0400

    updating file

--- src/week6Assignment/BankOperations.java
+++ src/week6Assignment/BankOperations.java
@@ -2,49 +2,7 @@ package week6Assignment;
 
 import java.util.Scanner;
 
-public class BankOperations extends AccountDetails {
-	private double updatedbalance = this.totalBalance;
-	private double withdraw;
-	private int deposit;
-	Scanner sc = new Scanner(System.in);
+public abstract class BankOperations {
+	public abstract void changePinPassword();
 
-	protected void withdraw() {
-		
-		System.out.println("Enter the money to be withdrawn");
-		withdraw= sc.nextInt();
-		if (updatedbalance >= withdraw) {
-
-			updatedbalance = updatedbalance - withdraw;
-			System.out.println("Please collect your money");
-			
-		} else
-			
-			System.out.println("Insufficient Balance");
-	}
-
-	protected void deposit() {
-		
-		System.out.println("Enter the amount to be deposited");
-		deposit =sc.nextInt();
-		
-		updatedbalance = updatedbalance + deposit;
-		
-		System.out.println("Your Money has been successfully depsited");
-		
-	}
-
-	protected void viewBalance() {
-		
-		System.out.println("Balance : " + updatedbalance);
-	}
-
-
-		
-	}
-
-	
-	
-		
-	
-
-
+}
