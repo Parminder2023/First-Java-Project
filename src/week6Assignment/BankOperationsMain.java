01d372477f1cfed8f54cfea34b0a13697a9599e8
Parminder2023 <raiparminder3663@.com>
2023-05-08 13:11:00 -0400

    updating file

--- src/week6Assignment/BankOperationsMain.java
+++ src/week6Assignment/BankOperationsMain.java
@@ -1,12 +1,31 @@
 package week6Assignment;
 
+import java.util.Scanner;
+
 public class BankOperationsMain {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
-		ATM Ao = new ATM();
-		Ao.ATMtransaction();
 		
+		System.out.println("choose 1 for ATM");
+		System.out.println("choose 2 for changePInPassword");
 
+		BankOperations bc = null;
+
+		int choice = sc.nextInt();
+
+		switch (choice) {
+		case 1:
+			bc = new ATM();
+
+			break;
+		case 2:
+			bc = new OnlineBanking();
+			break;
+
+		}
+		bc.changePinPassword();
+		
 	}
 }
