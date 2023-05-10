f596385b7c695db1671f07a05193358cd118b832
Parminder2023 <raiparminder3663@.com>
2023-05-09 21:33:38 -0400

    adding a file

--- src/week6Assignment/ATM.java
+++ src/week6Assignment/ATM.java
@@ -6,8 +6,7 @@ public class ATM extends BankOperations {
 
 	int enteredPIN;
 	int correctPIN = 1234;
-	int oldPIN=1234;
-	int updatedPIN;
+	int oldPIN = 1234;
 	int choice;
 
 	@Override
@@ -15,31 +14,38 @@ public class ATM extends BankOperations {
 	public void changePinPassword() {
 		Scanner sc = new Scanner(System.in);
 		int counter = 3;
-		
+
 		while (counter != 0) {
 			System.out.println("enter your 4 digit PIN : ");
 			int enteredPIN = sc.nextInt();
 
 			if (enteredPIN == correctPIN) {
 				System.out.println("update your PIN");
-				
-				int updatedPIN=sc.nextInt();
-				
-				if(updatedPIN==oldPIN);
-				{
-				System.out.println("updated PIN is same as : " + oldPIN);
-	              
-			}
-			}
-	               else {
-				
-				System.out.println("Invalid PIN");
 
-	               }break;
+				int updatedPIN = sc.nextInt();
+
+				if (updatedPIN == oldPIN) {
+					System.out.println("updated PIN is same as oldPIN");
+				}
+			} else {
+				System.out.println("Invalid PIN");
+			}
+			break;
 		}
-		if(counter==0) {
+		if (counter == 0) {
 			System.out.println("your account is locked");
 		}
 	}
-}
 
+	@Override
+	public double withdraw(double withdraw, double totalBalance) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double deposit(double deposit, double totalBalance) {
+		return 0;
+	}
+
+}
