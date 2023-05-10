0cf10b1966b11e3d2bd6e63bc63d81164a334e15
Parminder2023 <raiparminder3663@.com>
2023-05-10 09:27:26 -0400

    updating a file

--- src/week6Assignment/BankOperationsMain.java
+++ src/week6Assignment/BankOperationsMain.java
@@ -5,11 +5,12 @@ import java.util.Scanner;
 public class BankOperationsMain {
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
-		AccountDetails acc = new AccountDetails();
+		PersonAccount acc = new PersonAccount();
 		
 		System.out.println("choose 1 for ATM");
-		System.out.println("choose 2 for changePInPassword");
+		System.out.println("choose 2 for onlineBanking");
          
 		BankOperations bc = null;
 
@@ -18,52 +19,47 @@ public class BankOperationsMain {
 		switch (choice) {
 		case 1:
 			bc = new ATM();
+			bc.changePinPassword();
 
 			break;
 		case 2:
 			bc = new OnlineBanking();
+			bc.changePinPassword();
 			break;
-
+			
 		}
-		System.out.println("Enter account number");
-		int accNumber = sc.nextInt();
-		acc.setAccountNumber(accNumber);
-		
-		System.out.println("Enter name");
-		String name = sc.next();
-		acc.setNameOfAccountholder(name);
-		
-		System.out.println("Enter Total balance");
-		double totalBalance = sc.nextDouble();
-		acc.setTotalBalance(totalBalance);
-		
-		System.out.println("Enter PIN");
-		int pin = sc.nextInt();
-		acc.setPIN(pin);
-		
-		System.out.println("Enter online banking password");
-		String password = sc.next();
-		acc.setOnlineBankingPassword(password);
-		
-		System.out.println("Enter withdraw amount");
-		double withdrawAmount = sc.nextDouble();
-		
-		System.out.println("Enter amount to be deposit");
-		double depositAmount = sc.nextDouble();
-		OnlineBanking onb = new OnlineBanking();
-		
-		
-		double updatedBalance = onb.withdraw(withdrawAmount, totalBalance);
-		System.out.println("Your updated Balance is: "+updatedBalance);
-        
-		double updatedBalance1 = onb.deposit(depositAmount, totalBalance);
-		System.out.println("Your updated Balance is: "+updatedBalance1);
-        
 		
 
-
-
-		bc.changePinPassword();
+		BankOperations bankOperation = new ATM();
+		
+		Scanner sc1 = new Scanner(System.in);
+		
+		PersonAccount parminderAccount  = new PersonAccount("p1234",5000,1212,"p3663");
+		
+		PersonAccount akkiAccount = new PersonAccount("M464564", 10000, 001, "a2072");
+		
+		PersonAccount inayaitAccount = new PersonAccount("M464564", 6000, 1234, "i1234");
+		
+		PersonAccount kamalAccount = new PersonAccount("M464564", 7000, 2030, "kaur001");
+		
+		PersonAccount tannuAccount = new PersonAccount("M464564", 8000, 4020, "t0101");
+		
+		
+		
+		System.out.println("Enter the amount to be withdrawn");
+		double withdrawAmount = sc1.nextDouble();
+		
+		bankOperation.withdrawMoney(parminderAccount, withdrawAmount);
+		
+		System.out.println(parminderAccount.getBalance());
+		
 		
 	}
+	
+	
+
 }
+	
+	
+
+//		
