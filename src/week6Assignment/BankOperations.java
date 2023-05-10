3659cfd43309de2384b95879aa03e2a057c15e19
Parminder2023 <raiparminder3663@.com>
2023-05-09 21:36:23 -0400

    adding a file

--- src/week6Assignment/BankOperations.java
+++ src/week6Assignment/BankOperations.java
@@ -3,6 +3,10 @@ package week6Assignment;
 import java.util.Scanner;
 
 public abstract class BankOperations {
+
 	public abstract void changePinPassword();
 
+	public abstract double withdraw(double withdraw, double totalBalance);
+
+	public abstract double deposit(double deposit, double totalBalance);
 }
