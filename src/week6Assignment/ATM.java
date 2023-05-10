a28f53a75f9647a5c5912f935a99bc836509a0d9
Parminder2023 <raiparminder3663@.com>
2023-05-10 10:34:53 -0400

    adding a file

--- src/week6Assignment/ATM.java
+++ src/week6Assignment/ATM.java
@@ -5,34 +5,35 @@ import java.util.Scanner;
 public class ATM extends BankOperations {
 
 	Scanner sc = new Scanner(System.in);
-	@Override
-	public void changePinPassword() {
-		
-		
-	}
-
 	
-	public void changePinPassword(PersonAccount account) {
+
 		
-		System.out.println("Enter new ATM pin number");
-		int newPin = sc.nextInt();
-
-		while (newPin == account.getPIN() || (String.valueOf(newPin).length() < 4)) {
-
-			if (newPin == account.getPIN()) {
-				System.out.println("Entered pin is same as old pin. Please use different pin");
-			}
-			if (String.valueOf(newPin).length() < 4) {
-				System.out.println("Entered pin should have atleast 4 characters. Please try again!");
-			}
+		
+		
+		public void changePinPassword(PersonAccount account) {
+			
 			System.out.println("Enter new ATM pin number");
-			newPin = sc.nextInt();
+			int newPin = sc.nextInt();
+
+			while (newPin == account.getPIN() || (String.valueOf(newPin).length() < 4)) {
+
+				if (newPin == account.getPIN()) {
+					System.out.println("Entered pin is same as old pin. Please use different pin");
+				}
+				if (String.valueOf(newPin).length() < 4) {
+					System.out.println("Entered pin should have atleast 4 characters. Please try again!");
+				}
+				System.out.println("Enter new ATM pin number");
+				newPin = sc.nextInt();
+
+			}
+			System.out.println("Updated pin is : " + newPin);
 
 		}
-		System.out.println("Updated pin is : " + newPin);
 
-	}
-
+		
+		
+	
 	
 	
 }
