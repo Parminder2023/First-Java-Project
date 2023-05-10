8add983e6d40da4d788499abea5cce761611ea5c
Parminder2023 <raiparminder3663@.com>
2023-05-10 09:31:31 -0400

    updating a file

---
+++ src/week6Assignment/PersonAccount.java
@@ -0,0 +1,42 @@
+package week6Assignment;
+
+public class PersonAccount{
+	private String accountNumber;
+	private double balance;
+	private int PIN;
+	private String password;
+	public PersonAccount(String string, int i, int j, String string2) {
+		
+	}
+	public PersonAccount() {
+		// TODO Auto-generated constructor stub
+	}
+	public void PersonAccount(String string, int i, int j, String string2) {
+		// TODO Auto-generated constructor stub
+	}
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	public int getPIN() {
+		return PIN;
+	}
+	public void setPIN(int pIN) {
+		PIN = pIN;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
