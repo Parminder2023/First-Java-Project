9ec161c26232e00b71f4bec07d2161e01ec33786
Parminder2023 <raiparminder3663@.com>
2023-05-10 10:36:05 -0400

    adding a file

--- src/week6Assignment/BankOperations.java
+++ src/week6Assignment/BankOperations.java
@@ -19,9 +19,30 @@ public abstract class BankOperations {
 		
 	}
 
-	public abstract void changePinPassword();
+	public  void changePinPassword(PersonAccount account) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter new ATM pin number");
+		int newPin = sc.nextInt();
+
+		while (newPin == account.getPIN() || (String.valueOf(newPin).length() < 4)) {
+
+			if (newPin == account.getPIN()) {
+				System.out.println("Entered pin is same as old pin. Please use different pin");
+			}
+			if (String.valueOf(newPin).length() < 4) {
+				System.out.println("Entered pin should have atleast 4 characters. Please try again!");
+			}
+			System.out.println("Enter new ATM pin number");
+			newPin = sc.nextInt();
+
+		}
+		System.out.println("Updated pin is : " + newPin);
+
+	}
 		
 	}
+		
+	
 		
 
 	
